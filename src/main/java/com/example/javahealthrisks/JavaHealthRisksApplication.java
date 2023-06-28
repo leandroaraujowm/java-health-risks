9f2e4eaba23e60d5d933430bf14d26107132fd83
leandroaraujowm <leandro.araujowm@gmail.com>
2023-06-28 11:44:29 -0300

    feat: :sparkles: Add development enviroment

---
+++ src/main/java/com/example/javahealthrisks/JavaHealthRisksApplication.java
@@ -0,0 +1,13 @@
+package com.example.javahealthrisks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaHealthRisksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaHealthRisksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javahealthrisks/JavaHealthRisksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javahealthrisks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaHealthRisksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
