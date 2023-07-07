8f8f9ee18093b3dfff6c4bf44d694f63626ac74a
leandroaraujowm <leandro.araujowm@gmail.com>
2023-07-07 11:07:56 -0300

    style: :fire: Remove RiskCustomerModal & repository

--- src/main/java/com/example/javahealthrisks/repositories/RiskCustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.javahealthrisks.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.javahealthrisks.models.RiskCustomerModel;
-
-public interface RiskCustomerRepository extends JpaRepository<RiskCustomerModel, Long> {
-
-}
